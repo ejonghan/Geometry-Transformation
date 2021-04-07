8f5b41ce3907a7fa0d4a719072350dcbc7f23219
JongHan Lee <62053466+ejonghan@users.noreply.github.com>
2021-04-07 20:39:02 +0900

    Initial Commit

---
+++ forward/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  forward
+//
+//  Created by 리종한 on 2021/04/07.
+//  Copyright © 2021 리종한. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
